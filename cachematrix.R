25ee0f7cd83281bb40220b3f8f460264b6455683
unknown <jvb04002@gmail.com>
2015-02-20 01:12:49 -0800

    123

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
-## Write a short comment describing this function
+# 1.set the value of the matrix
+# 2.get the value of the matrix
+# 3.set the inverse matrix of the matrix
+# 4.get the inverse matrix of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(matrix) m <<- matrix
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse matrix  of the special 
+## "vector" created with the above function. However, it first checks if the
+## calculation is saved in cache from before.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setmatrix(m)
+  m
 }
+
+## Return a matrix that is the inverse of 'x'. Make sure to use nested fuctions.
+
+#Example:
+# x
+#[,1]  [,2]
+#[1,]  1.00 -0.25
+#[2,] -0.25  1.00
+#> solve(x)
+#[,1]      [,2]
+#[1,] 1.0666667 0.2666667
+#[2,] 0.2666667 1.0666667
+#> cacheSolve(makeCacheMatrix(x))
+#[,1]      [,2]
+#[1,] 1.0666667 0.2666667
+#[2,] 0.2666667 1.0666667
